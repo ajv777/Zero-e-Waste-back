47a3c9bfe586ffc0a5aa65e5e6d9457460d5e1fc
pgarcia92 <palomaglorenzo@gmail.com>
2020-06-22 17:32:46 +0200

    implement images

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const apiItemsRouter = require('./api/items');
 const apiUsersRouter = require('./api/users');
 const apiCatsRouter = require('./api/cats')
+const recycleRouter = require('./api/recycle')
 
 // Compruebo que se conecta la ruta
 /* router.get('/', (req,res) => {
@@ -13,5 +14,6 @@ const {checkToken, multipartMW} = require('./middlewares')
 router.use('/items', checkToken, multipartMW, apiItemsRouter); // he quitado el /api/ porque ya estamos en api y por ello no es necesario
 router.use('/users', apiUsersRouter);
 router.use('/cats', checkToken, apiCatsRouter)
+router.use('/recycle', recycleRouter)
 
 module.exports = router;
